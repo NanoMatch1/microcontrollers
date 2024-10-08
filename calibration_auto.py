4f5443acf3e21e8d75fea7162146b4ed3172fc57
match@raman <match@Raman>
2024-10-09 10:14:12 +1100

    feat:add dataclass for peak class

--- calibration_auto.py
+++ calibration_auto.py
@@ -1,13 +1,386 @@
 import os
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
+import csv
 import json
+import scipy.optimize as opt
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
+
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)), 'analysis-spectroscopy','analysis_spectroscopy'))
 
-# basename = os.path.dirname(os.path.dirname(__file__))
-# sys.path.append(os.path.join(basename, 'analysis-spectroscopy'))
 from analysis_spectroscopy import dataset_analysis as asp
+
+
+'''make a quick plot of calibration data'''
+
+'''eventually, create a calibration routine that goes to each wavelength and sweeps the laser frequency across the ccd. Collect spectra, peakfit, extract peak centre, and use to calibrate more efficiently.'''
+
+'''
+from most recent calibration
+370-751 (delta 400 steps on l1) (558 (d 200))
+857.44,,,-3473,405000,414
+872.646,,,-4473,415000,152
+pix_diff = 262 pix = 4.1793 nm
+
+currently 410000,558
+
+delta = 15.206/-1000 (nm_per_laser_steps)
+15.206-4.1793 nm = 11.0267 nm
+11.0267 nm/10000 steps on TRIAX = 0.00110267 nm/step
+'''
+
+
+
+calibration_records = {
+    'date': '2024-08-27'
+    ,
+
+    'manual_measurements': {
+        'nm_per_laser_step': 0.0158,
+        'lamba_change_for_400_steps': 6.32,
+        'nm_per_pixel': 4/400, # retrieved from recent calibration sweep
+        # nm_per_step = 0
+        'triax_steps': 410000,
+        'nm_per_triax_step': 0.00110267
+    },
+
+    'eept': {
+        'headers': {
+            'L1': 0,
+            'L2': 1,
+            'NIU': 2,
+            'NIU': 3,
+            'G1': 4,
+            'G2': 5,
+            'NIU': 6,
+            'NIU': 7,
+            'triax_steps': 8,
+            'pixels': 9
+        },
+        'data':
+            "['X-473', 'Y212', 'Z0', 'A0']:['X58', 'Y45', 'Z0', 'A0']:380000,518\n['X-973', 'Y424', 'Z0', 'A0']:['X112', 'Y90', 'Z0', 'A0']:385000,390\n['X-1473', 'Y636', 'Z0', 'A0']:['X184', 'Y170', 'Z0', 'A0']:390000,263\n['X-2473', 'Y1058', 'Z0', 'A0']:['X350', 'Y359', 'Z0', 'A0']:395000,645\n['X-3473', 'Y1479', 'Z0', 'A0']:['X508', 'Y587', 'Z0', 'A0']:405000,414\n['X-4473', 'Y1897', 'Z0', 'A0']:['X619', 'Y698', 'Z0', 'A0']:415000,152\n['X-5473', 'Y2327', 'Z0', 'A0']:['X770', 'Y851', 'Z0', 'A0']:420000,542\n['X-6473', 'Y2735', 'Z0', 'A0']:['X917', 'Y970', 'Z0', 'A0']:430000,300\n['X-7473', 'Y3149', 'Z0', 'A0']:['X1023', 'Y1064', 'Z0', 'A0']:435000,668\n['X-8473', 'Y3572', 'Z0', 'A0']:['X1163', 'Y1181', 'Z0', 'A0']:445000,436\n['X27', 'Y1', 'Z0', 'A0']:['X-28', 'Y-28', 'Z0', 'A0']:375000,646\n['X1027', 'Y-413', 'Z0', 'A0']:['X-198', 'Y-224', 'Z0', 'A0']:370000,274\n['X2027', 'Y-846', 'Z0', 'A0']:['X-323', 'Y-343', 'Z0', 'A0']:360000,489\n['X3027', 'Y-1262', 'Z0', 'A0']:['X-468', 'Y-455', 'Z0', 'A0']:355000,140\n['X4027', 'Y-1687', 'Z0', 'A0']:['X-603', 'Y-566', 'Z0', 'A0']:345000,367\n#['X5027', 'Y-2186', 'Z0', 'A0']:['X-693', 'Y-650', 'Z0', 'A0']:335000,589\n#['X6027', 'Y-2609', 'Z0', 'A0']:['X-848', 'Y-782', 'Z0', 'A0']:330000,242\n#['X7027', 'Y-3045', 'Z0', 'A0']:['X-963', 'Y-888', 'Z0', 'A0']:320000,473\n#['X-473', 'Y168', 'Z0', 'A0']:['X62', 'Y50', 'Z0', 'A0']:380000,515"
+    }
+}
+
+
+
+# nm_per_pixel = 6.38064/400 # retrieved from recent calibration sweep
+# # nm_per_step = 0
+# triax_steps = 375000
+# nm_per_triax_step = 0.00110267
+
+# absolute = 802.5-(643*nm_per_pixel)
+# new = absolute + (nm_per_triax_step*(410000-375000)) + (nm_per_pixel*558)
+
+#662/469
+
+#731.162-734.432 = -3.27 nm
+#-3.27nm/200steps = -0.01635 nm/step
+#293-660pix=-367
+#0.0168557nm/pix*-367pix = -6.186 nm/3000 steps triax
+# -0.002062 nm/step triax
+
+
+def simple_sin_fit(xvalues, a, b, c, d):
+    return a * np.sin(b * xvalues + c) + d
+
+def polynomial_fit(xvalues, a, b, c):
+    return a*xvalues**2 + b*xvalues + c
+
+def poly_sin_modulation_fit(x, a2, a1, a0, A, B, C, D):
+    # Polynomial part
+    poly = a2 * x**2 + a1 * x + a0
+    # Sinusoidal modulation part
+    modulation = A * np.sin(B * x + C) + D
+    return poly + modulation
+
+def review_report():
+    with open(os.path.join(os.path.dirname(__file__), 'calibration_report.json'), 'r') as f:
+        data = json.load(f)
+        for key, value in data.items():
+            print(key)
+            for k, v in value.items():
+                print(k)
+                print(v)
+                # print('\n')
+
+def r_squared(y_true, y_pred):
+    '''Calculate R^2 (coefficient of determination) for a regression model.'''
+    ss_res = np.sum((y_true - y_pred) ** 2)  # Residual sum of squares
+    ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)  # Total sum of squares
+    return 1 - (ss_res / ss_tot)
+
+def rmse(y_true, y_pred):
+    '''Calculate the root mean squared error for a regression model.'''
+    return np.sqrt(np.mean((y_true - y_pred) ** 2))
+
+def mae(y_true, y_pred):
+    '''Calculate the mean absolute error for a regression model.'''
+    return np.mean(np.abs(y_true - y_pred))
+
+def residual_std(residuals):
+    '''Calculate the standard deviation of residuals for a regression model.'''
+    return np.std(residuals)
+
+def adjusted_r_squared(y_true, y_pred, p):
+    '''Calculate the adjusted R^2 (coefficient of determination) for a regression model.'''
+    n = len(y_true)
+    r2 = r_squared(y_true, y_pred)
+    return 1 - (1 - r2) * (n - 1) / (n - p - 1)
+
+@dataclass
+class FitMetrics:
+    '''Dataclass for fit quality metrics.'''
+    r2: int
+    rmse: int
+    mae: int
+    res_std: int
+
+@dataclass
+class Peak:
+    '''Dataclass for peak data.'''
+    peak_type: str
+    pos: float
+    amp: float
+    fwhm: float
+    eta: float = 0
+
+    def __repr__(self):
+        return f'Peak D-Class(pos={round(self.pos, 2)}, amp={round(self.amp, 2)}, fwhm={round(self.fwhm, 2)}, eta={round(self.eta, 2)})'
+
+class Calibration:
+
+    calib_dict = {
+    'pixels_per_laser_step': -194/200,
+    'nm_per_laser_step': -0.01635,
+    'nm_per_pixel': -0.01635/-0.97, #0.0168557
+
+    # 'nm_per_pixel': -6.32/400, # retrieved from recent calibration sweep
+    # nm_per_step = 0
+    'triax_steps': 375000,
+    'nm_per_triax_step': -0.002062
+    }
+
+    initial_fit_parameters = {
+        'wl_to_g1': {'poly': [0.001, 8, -0.007],
+                     'sin': [-17.06930504,   0.08537372, -27.92235843,   0.70444867]},
+        'g1_to_wl': {'poly': [0.00, 0.1, 800],
+                    'sin': [-1.53840861,  0.00463291, -1.04822887, -0.15844918]},
+        'wl_to_g2': {'poly': [0.001, -0.4, 850],
+                     'sin': [5.87970348,   0.08537372, -27.92235843,   0.70444867]},
+        'g2_to_wl': {'poly': [0.00, 1, 600],
+                    'sin': [-1.07, 0.04, -7, 0]},
+
+        }
+    
+    lower_bound = [-50, 0.075, -150, -100]
+    upper_bound = [50, 0.1, 150, 100]
+    
+
+    def __init__(self, calibration_dict:dict, showplots=False):
+        # self.data = data
+        self.dataDir = os.path.join(os.path.dirname(__file__), 'laser_calibration')
+        self.files = [f for f in os.listdir(self.dataDir) if f.endswith('.txt')]
+        self.showplots = showplots
+        self.calibration_metrics = {}
+        self.calibrations = {}
+        self.report_dict = {'initial': {}, 'subtractive': {}, 'additive': {}}
+        self.process_calibration_data(calibration_dict)
+
+    def process_calibration_data(self, calibration_dict):
+        data_array = [[key, peak.pos] for key, peak in calibration_dict.items()]
+        print('data_array')
+        breakpoint()
+
+    def calculate_fit_metrics(self, actual, model):
+        # Fit quality metrics
+        r2 = r_squared(actual, model)
+        rmse_val = rmse(actual, model)
+        mae_val = mae(actual, model)
+        residuals = actual - model
+        res_std = residual_std(residuals)
+
+        return FitMetrics(r2, rmse_val, mae_val, res_std)
+
+    def save_report(self):
+        # unpack report_dict into dict for json serialization
+        newDict = {x: y for x, y in self.report_dict.items()}
+        for mode, data in self.report_dict.items():
+            for key, value in data.items():
+                newDict[mode][key] = (value[0].__dict__, value[1])
+
+        with open(os.path.join(os.path.dirname(__file__), 'calibration_report.json'), 'w') as f:
+            json.dump(newDict, f)
+    
+
+    def save_calibration(self, filename):
+        with open(os.path.join(os.path.dirname(__file__), '{}_autocal.json'.format(filename)), 'w') as f:
+            json.dump(calibration.calibrations, f)
+
+        print("Calibration complete: Successfully saved calibration data to 'calibrations.json' file.")
+
+    def wavelength_to_g1(self, poly_order=2, mode=None, show=False, polysin=False):
+        '''Calibration for using laser wavelength to calculate G1 steps.'''
+
+
+
+        assert mode in ['subtractive', 'additive'], 'Invalid mode. Must be either "subtractive" or "additive".'
+
+        if mode == 'subtractive':
+            wavelength_axis = self.laser_wavelength_axis
+            print('using laser wavelength axis')
+        elif mode == 'additive':
+            wavelength_axis = self.grating_wavelength_axis
+            print('using grating wavelength axis')
+
+        if polysin is True:
+            print('Fitting with polysin')
+            initial_guess = [0.01, -5, 5000, 200, 0.05, 0, 100]
+            # fit_coeff_wavelength_to_g1 = poly_sin_modulation_fit(wavelength_axis, *initial_guess)
+            fit_coeff_wavelength_to_g1, pcov = opt.curve_fit(poly_sin_modulation_fit, wavelength_axis, g1_steps, p0=initial_guess)
+            p_wavelength_to_g1 = lambda x: poly_sin_modulation_fit(x, *fit_coeff_wavelength_to_g1)
+        else:
+            fit_coeff_wavelength_to_g1 = np.polyfit(wavelength_axis, g1_steps, poly_order)
+            p_wavelength_to_g1 = np.poly1d(fit_coeff_wavelength_to_g1)
+
+        y_pred = p_wavelength_to_g1(wavelength_axis)
+        residuals = g1_steps - y_pred
+
+        # Fit quality metrics
+        fit_metrics = self.calculate_fit_metrics(g1_steps, y_pred)
+        self.report_dict[mode]['wl_to_g1_{}'.format(mode)] = (fit_metrics, fit_coeff_wavelength_to_g1.tolist())
+
+        if show is True or self.showplots is True:
+            fig, ax = plt.subplots(2, 1)
+            ax[0].scatter(wavelength_axis, g1_steps, label='G1 Steps')
+            ax[0].plot(wavelength_axis, p_wavelength_to_g1(wavelength_axis), label='G1 Steps fit', color='tab:purple')
+            residuals = g1_steps - p_wavelength_to_g1(wavelength_axis)
+            ax[1].plot(wavelength_axis, residuals, label='G1 Steps residuals', marker='o')
+            ax[0].set_title('Wavelength to G1 Steps')
+            ax[0].legend()
+            ax[1].legend()
+            plt.show()
+
+        self.calibration_metrics['wl_to_g1_{}'.format(mode)] = fit_metrics
+        self.calibrations['wl_to_g1_{}'.format(mode)] = fit_coeff_wavelength_to_g1.tolist()
+
+        return fit_coeff_wavelength_to_g1, fit_metrics
+        
+    def g1_to_wavelength(self, g1_steps, poly_order=2, mode=None, show=False):
+        '''Reverse calibration for calculating laser wavelength from G1 steps.'''
+
+        assert mode in ['subtractive', 'additive'], 'Invalid mode. Must be either "subtractive" or "additive".'
+
+        if mode == 'subtractive':
+            wavelength_axis = self.laser_wavelength_axis
+            print('using laser wavelength axis')
+        elif mode == 'additive':
+            wavelength_axis = self.grating_wavelength_axis
+            print('using grating wavelength axis')
+
+        fit_coeff_g1_to_wavelength = np.polyfit(g1_steps, wavelength_axis, poly_order)
+        p_g1_to_wavelength = np.poly1d(fit_coeff_g1_to_wavelength)
+
+        y_pred = p_g1_to_wavelength(g1_steps)
+        residuals = wavelength_axis - y_pred
+
+        # Fit quality metrics
+        fit_metrics = self.calculate_fit_metrics(wavelength_axis, y_pred)
+        self.report_dict[mode]['g1_to_wl_{}'.format(mode)] = (fit_metrics, fit_coeff_g1_to_wavelength.tolist())
+
+        if show is True or self.showplots is True:
+            fig, ax = plt.subplots(2, 1)
+            ax[0].scatter(g1_steps, wavelength_axis, label='Wavelength')
+            ax[0].plot(g1_steps, p_g1_to_wavelength(g1_steps), label='Wavelength fit', color='tab:purple')
+            residuals = wavelength_axis - p_g1_to_wavelength(g1_steps)
+            ax[1].plot(g1_steps, residuals, label='Wavelength residuals', marker='o')
+            ax[0].set_title('G1 Steps to Wavelength')
+            ax[0].legend()
+            ax[1].legend()
+            plt.show()
+
+        self.calibration_metrics['g1_to_wl_{}'.format(mode)] = fit_metrics
+        self.calibrations['g1_to_wl_{}'.format(mode)] = fit_coeff_g1_to_wavelength.tolist()
+        
+        return fit_coeff_g1_to_wavelength, fit_metrics
+        
+    def wavelength_to_g2(self, g2_steps, poly_order=2, mode=None, show=False):
+        '''Calibration for using laser wavelength to calculate G2 steps.'''
+
+        assert mode in ['subtractive', 'additive'], 'Invalid mode. Must be either "subtractive" or "additive".'
+
+        if mode == 'subtractive':
+            wavelength_axis = self.laser_wavelength_axis
+            print('using laser wavelength axis')
+        elif mode == 'additive':
+            wavelength_axis = self.grating_wavelength_axis
+            print('using grating wavelength axis')
+        
+        fit_coeff_wavelength_to_g2 = np.polyfit(wavelength_axis, g2_steps, poly_order)
+        p_wavelength_to_g2 = np.poly1d(fit_coeff_wavelength_to_g2)
+
+        y_pred = p_wavelength_to_g2(wavelength_axis)
+        residuals = g2_steps - y_pred
+
+        # Fit quality metrics
+        fit_metrics = self.calculate_fit_metrics(g2_steps, y_pred)
+        self.report_dict[mode]['wl_to_g2_{}'.format(mode)] = (fit_metrics, fit_coeff_wavelength_to_g2.tolist())
+
+        if show is True or self.showplots is True:
+            fig, ax = plt.subplots(2, 1)
+            ax[0].scatter(wavelength_axis, g2_steps, label='G2 Steps')
+            ax[0].plot(wavelength_axis, p_wavelength_to_g2(wavelength_axis), label='G2 Steps fit', color='tab:purple')
+            residuals = g2_steps - p_wavelength_to_g2(wavelength_axis)
+            ax[1].plot(wavelength_axis, residuals, label='G2 Steps residuals', marker='o')
+            ax[0].set_title('Wavelength to G2 Steps {}'.format(mode))
+            ax[0].legend()
+            ax[1].legend()
+            plt.show()
+
+        self.calibration_metrics['wl_to_g2_{}'.format(mode)] = fit_metrics
+        self.calibrations['wl_to_g2_{}'.format(mode)] = fit_coeff_wavelength_to_g2.tolist()
+
+        return fit_coeff_wavelength_to_g2, fit_metrics
+    
+    def g2_to_wavelength(self, g2_steps, poly_order=2, mode=None, show=False):
+        '''Reverse calibration for calculating laser wavelength from G2 steps.'''
+
+        assert mode in ['subtractive', 'additive'], 'Invalid mode. Must be either "subtractive" or "additive".'
+
+        if mode == 'subtractive':
+            wavelength_axis = self.laser_wavelength_axis
+            print('using laser wavelength axis')
+        elif mode == 'additive':
+            wavelength_axis = self.grating_wavelength_axis
+            print('using grating wavelength axis')
+
+        fit_coeff_g2_to_wavelength = np.polyfit(g2_steps, wavelength_axis, poly_order)
+        p_g2_to_wavelength = np.poly1d(fit_coeff_g2_to_wavelength)
+
+        y_pred = p_g2_to_wavelength(g2_steps)
+        residuals = wavelength_axis - y_pred
+
+        # Fit quality metrics
+        fit_metrics = self.calculate_fit_metrics(wavelength_axis, y_pred)
+        self.report_dict[mode]['g2_to_wl_{}'.format(mode)] = (fit_metrics, fit_coeff_g2_to_wavelength.tolist())
+
+        if show is True or self.showplots is True:
+            fig, ax = plt.subplots(2, 1)
+            ax[0].scatter(g2_steps, wavelength_axis, label='Wavelength')
+            ax[0].plot(g2_steps, p_g2_to_wavelength(g2_steps), label='Wavelength fit', color='tab:purple')
+            residuals = wavelength_axis - p_g2_to_wavelength(g2_steps)
+            ax[1].plot(g2_steps, residuals, label='Wavelength residuals', marker='o')
+            ax[0].set_title('G2 Steps to Wavelength {}'.format(mode))
+            ax[0].legend()
+            ax[1].legend()
+            plt.show()
+
+        self.calibration_metrics['g2_to_wl_{}'.format(mode)] = fit_metrics
+        self.calibrations['g2_to_wl_{}'.format(mode)] = fit_coeff_g2_to_wavelength.tolist()
+
+        return fit_coeff_g2_to_wavelength, fit_metrics
+
 
 def sort_by_key(value):
     return int(value.split('_')[1])
@@ -16,8 +389,6 @@ def get_latest_calibration_file(dataDir):
     fileList = [file for file in os.listdir(os.path.join(os.path.dirname(__file__), 'autocalibration')) if file.endswith('.json')]
     sorted_files = sorted(fileList, key=lambda x: int(x.split('_')[1]))
     return sorted_files[-1]
-
-# note: only works on one file at a time
 
 
 class Peaks:
@@ -78,7 +449,7 @@ def generate_autocal(scriptDir, dataDir):
         # print(wave, peaks)
         for peak in peaks:
             # breakpoint()
-            peak_list.append(Peaks(*peak))
+            peak_list.append(Peak(*peak))
         if len(peak_list) > 1:
             peak_list.sort(key=lambda x: x.amp)
         peak = peak_list[0]
@@ -86,7 +457,8 @@ def generate_autocal(scriptDir, dataDir):
 
     for key, value in calibration_peaks.items():
         print(key, value)
-    breakpoint()
+    # breakpoint()
+    calibration = Calibration(calibration_peaks, showplots=True)
         
 
 

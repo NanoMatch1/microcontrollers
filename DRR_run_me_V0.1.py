2b65c4941f31dcc1f43510c11fa0dd4f091b2729
match@raman <match@Raman>
2025-01-24 14:44:16 +1100

    refactor:remove redundant and commented code

--- DRR_run_me_V0.1.py
+++ DRR_run_me_V0.1.py
@@ -130,8 +130,7 @@ class Microscope:
         self.pinhole = None
         self.save_pinhole = None
         self.detector_safety = True
-        # self.current_motor_positions = {'A': {}, 'B': {}}
-        # self.to_addivite = -12990 -13108+39 
+
 
         self.acq_time = 1
         self.centre_wavelength = 376886
@@ -156,7 +155,6 @@ class Microscope:
             'sd': self.go_to_grating_wavelength,
             'st': self.go_to_triax_wavelength,
             'sall': self.go_to_wavelength_all,
-            # 'shift': self.go_to_grating_wavelength,
             'wai': self.get_all_current_positions,
             'reference': self.reference_calibration, # TODO: Bug where multiple calls are needed to refresh. Looks like grating motors are one step behind.
             'shift': self.go_to_wavenumber,
@@ -184,13 +182,6 @@ class Microscope:
             'acquire': self.acquire_spectrum,
             'run': self.continuous_acquire,
             'stop': self.stop_continuous_acquire,
-
-            # 'testacq': self.test_acquire_series,
-
-            # 'changemode': self.change_monochromator_mode,
-            # 'setpos': self.set_absolute_positions
-            # additive reference at 220: [83, -13108...]
-
         }
 
         self.camera_dict = {
@@ -199,7 +190,6 @@ class Microscope:
             'roi': self.camera_set_roi,
         }
 
-        # commands for controlling TRIAX spectrometer
         self.spectrometer_dict = {
             'init': 'A',
             'comsmode': '02000',
@@ -225,16 +215,6 @@ class Microscope:
             #'entrance mirror to side enterance': 'd0'
         }
 
-        # commands for sample/stage motion and imaging beamsplitter
-        self.stage_dict = {
-            # 'relative': 'relative',
-            # 'absolute': 'absolute',
-            # 'X': 'X',
-            # 'Y': 'Y',
-            # 'Z': 'Z',
-            # 'imagemode': 'G0 E-500',
-            # 'ramanmode': 'G0 E500'
-        }
 
         self.dual_mono_dict = {
             'add_mode': 'additive',
@@ -274,15 +254,9 @@ class Microscope:
 
         }
 
-        # additive position: Y -12984, subtractive = 0
-
-        self.acquisition_dict = {
-            # 'acq': 'acq', 
-            # 'run': 'run',
+        self.apd_dict = {
             'gsh': 'gsh',
             'rldr0': 'ld0'
-
-
         }
 
         self.laser_dict = {
@@ -1706,11 +1680,11 @@ class Microscope:
             self.general_dict[com[0]]()
             response = 'Connected to {}'.format(com[0])
 
-        elif com[0] in self.acquisition_dict.keys():
+        elif com[0] in self.apd_dict.keys():
             if len(com) > 1:
-                command = 'm{}{}m'.format(self.acquisition_dict[com[0]], com[1])
+                command = 'm{}{}m'.format(self.apd_dict[com[0]], com[1])
             else:
-                command = 'm{}m'.format(self.acquisition_dict[com[0]])
+                command = 'm{}m'.format(self.apd_dict[com[0]])
             print('UI>UNO:{}'.format(command))
             self.send_command_to_UNO(command)
             time.sleep(0.1)
@@ -2137,119 +2111,6 @@ class Microscope:
                 continue
 
 
-    
-
-    
-
-
-    
-    # def send_command_to_apd(self, command):
-    #     self.apd_serial.write('{}\r'.format(command).encode())
-    #     time.sleep(0.01)
-    #     # response = self.apd_serial.read(self.apd_serial.inWaiting())
-    #     # print(response)
-    #     # 
-
-    # def set_acquisition_time(self, acq_time):
-    #     self.send_command_to_apd('t{}\r'.format(acq_time))
-    #     time.sleep(0.1)
-    #     response = self.apd_serial.read(self.apd_serial.inWaiting())
-    #     print(response)
-    #     code = response.decode().strip('\r\n')
-    #     if code.startswith('aa'):
-    #         acq_time = float(code[2:])
-    #         print('Master Receive - Acquisition time set to: {}'.format(acq_time))
-    #     # 
-    #     time.sleep(1)
-    #     print(self.apd_serial.read(self.apd_serial.inWaiting()))
-
-
-
-
-    # def serial_connect(self, serial_port, baud_rate=115200):
-    #     try:
-    #         # Establish a serial connection
-    #         ser = serial.Serial(serial_port, baud_rate, timeout=1) 
-    #         # print(ser)
-    #         # ser.write('Test\r'.encode())
-    #         time.sleep(self.pico_read_delay)
-    #         response = ser.read(ser.inWaiting())
-    #         print(response)
-    #         return ser
-
-        # except serial.SerialException as e:
-        #     print(f"Error: {e}")
-
-        # finally:
-        #     if 'ser' in locals() and ser.is_open:
-        #         ser.close()
-        
-    # def connect_to_marlin(self):
-    #     ''' Current working pico-MARLIN-coms 19/01/24'''
-    #     # Replace with your serial port and baud rate
-    #     serial_port = 'COM4'  # or 'COM3' for Windows
-    #     baud_rate = 115200  # Adjust as per your Pico's settings
-    #     self.pico_read_delay = 0.1  # This is important! Some Picos need a delay before sending data
-
-    #     comList = {
-
-    #     }
-    #     def get_mode():
-
-    #         while True:
-    #             currentMode = input('Enter the current mode: ramanmode or imagemode')
-    #             if currentMode == 'ramanmode' or currentMode == 'imagemode':
-    #                 return currentMode
-
-    #     self.currentMode = get_mode()
-
-
-    #     try:
-    #         # Establish a serial connection
-    #         ser = serial.Serial(serial_port, baud_rate, timeout=1) 
-    #         # print(ser)
-    #         ser.write('Test\r'.encode())
-    #         time.sleep(self.pico_read_delay)
-    #         response = ser.read(ser.inWaiting())
-    #         print(response)
-    #         return ser
-
-    #     except serial.SerialException as e:
-    #         print(f"Error: {e}")
-
-    #     finally:
-    #         if 'ser' in locals() and ser.is_open:
-    #             ser.close()
-
-    # def send_command_to_pico(self, command):
-
-    #     if command[0] == 'flush':
-    #         self.pico_marlin.flush()
-    #         return
-    #     if command[0] == self.currentMode:
-    #         print('Error: alread in {} mode'.format(self.currentMode))
-    #         return
-    #     # command = command
-    #     if command[0] in self.pico_dict.keys():
-    #         if len(command) > 1:
-    #             command = '{}{}'.format(self.pico_dict[command[0]], command[1]) # concatenate command if parameters are provided
-    #         # command = self.pico_dict[command]
-    #         else:
-    #             command = self.pico_dict[command[0]]
-    #     self.pico_marlin.write(f'{command}\r'.encode())
-    #     self.pico_marlin.flush()
-
-    #     # Read the response
-    #     time.sleep(self.pico_read_delay)
-    #     response = self.pico_marlin.read(self.pico_marlin.inWaiting())  # Read available bytes
-    #     if response:
-    #         print("Response from Pico:")
-    #         print(response.decode().strip())
-    #     else:
-    #         print("No response received. Check the connection and settings.")
-
-
-
     def send_command_to_spectrometer(self, command, report=True):
                     # self.instrument.write(com)
                     # time.sleep(0.0001)
@@ -2289,78 +2150,6 @@ class Microscope:
         return self.spectrometer, self.state
 
 
-    # def main(self):
-    #     while True:
-    #         try:
-    #             # loop continuously here for com input
-    #             # response = self.apd_serial.read(self.apd_serial.inWaiting())
-    #             # response = self.uno_serial.read(self.uno_serial.inWaiting())
-    #             # print(response)
-    #             com = input('Enter command:\n')
-    #             # if com == 'COM':
-    #             #     while True:
-    #             #         com = 'Enter string command:\n'
-    #             #         # self.instrument.write(com)
-    #             #         # time.sleep(0.0001)
-    #             # else:
-    #             # command = com.split(' ')
-    #             self.send_command_to_UNO(com)
-    #             self.read_command_from_uno()
-
-    #             continue
-
-
-    #             if com[0] in self.APD_comList:
-    #                 self.send_command_to_apd(com)
-    #                 time.sleep(0.1)
-    #             elif com == 'read':
-    #                 response = self.apd_serial.read(self.apd_serial.inWaiting())
-    #                 print(response)
-    #             else:
-    #                 print('Command not recognized: {}'.format(com))
-                
-    #             continue
-
-
-    #             # if command[0] == 'read':
-    #             #     response = self.apd_serial.read(self.apd_serial.inWaiting())
-    #             #     print('APD:', response)
-    #             #     response = self.apd_serial.read(self.pico_marlin.inWaiting())
-    #             #     print('pico_marlin:', response)
-
-    #             # if command[0] == 't':
-    #             #     self.set_acquisition_time(command[1])
-
-    #                 # self.send_command_to_apd(struct.pack('<f', 0.12))
-    #                 # self.send_command_to_apd(struct.pack('<f', 12))
-    #                 # self.send_command_to_apd(struct.pack( .'<f', 45.6))
-
-    #                 # self.send_command_to_apd(2.5)
-    #                 # # time.sleep(0.001)
-    #                 # self.send_command_to_apd('run')
-    #                 # time.sleep(0.001)
-
-
-    #             if command[0] in self.spectrometer_dict.keys():
-    #                 self.send_command_to_spectrometer(command)
-    #             elif command[0] in self.apd_dict.keys():
-    #                 self.send_command_to_apd(self.apd_dict[command[0]])
-    #             else:
-    #                 self.send_command_to_pico(self.pico_dict[command[0]])
-    #         except Exception as e:
-    #             print(e) 
-
-                    
-def continuous():
-    root = tk.Tk()
-    gui = ArduinoInterface(root)
-
-    root.mainloop()
-
-def discon():
-    microscope = Microscope()
-    microscope.main()
-
 def cli():
     microscope = Microscope(debug_skip=['laser', 'TRIAX'], unoCOM='COM10')
     # microscope.cli_commands()
@@ -2372,118 +2161,4 @@ def cli():
         print(e)
 
 if __name__ == '__main__':
-    # discon()
-    # continuous()
     cli()
-
-
-# class Spectrometer:
-
-    def __init__(self):
-        self.spectrometer, spectrometer_state = self.connect_to_triax()
-
-    def connect_to_triax(self):
-        # Open a connection to the instrument
-        rm = pyvisa.ResourceManager()
-        rm.list_resources()
-        spectrometer = rm.open_resource('GPIB0::1::INSTR')  # Replace with the actual VISA address of your instrument
-        # instrument = rm.open_resource('COM6')  # Replace with the actual VISA address of your instrument
-        # s = serial.Serial(port='GPIB0::1::INSTR', timeout=5000)
-
-
-    # def connect(instrument):
-        spectrometer.write('O2000')
-        time.sleep(0.0001)
-        state = spectrometer.read()
-        spectrometer.write('WHERE AM I')
-        time.sleep(0.0001)
-        state = spectrometer.read()
-        print(state)
-        # 
-        return spectrometer, state
-
-
-
-
-
-    def control_instrument(self, command):
-        while True:
-            try:
-                # loop continuously here for com input
-                com = input('Enter command - c to continue:\n')
-                if com == 'c':
-                    break
-                spectrometer.write(com)
-                if com == 'A':
-                    count = 100
-                    while count > 0:
-                        print('Initialising: Sleeping for {} seconds'.format(count))
-                        time.sleep(1)
-                        count -= 1
-                response = spectrometer.read()
-                print('RES:', response)
-            except Exception as e:
-                print(e) 
-
-        count = 0
-        for x in range(10):
-            spectrometer.write('F0,{}'.format(5000))
-            response = spectrometer.read()
-            print('moving to {}'.format(x*5000))
-            # while response == 'b':
-            # while True:
-            #     # response = spectrometer.write('E')
-            #     time.sleep(0.5)
-            #     count+=1
-            #     if count > 50:
-            #         break
-            time.sleep(0.5)
-            # count += 1
-                # print('waiting')
-                
-                # response = spectrometer.read()
-                # if count > 50:
-                #     print('timed out')
-                #     break
-
-            
-
-            
-            
-
-        while True:
-            try:
-                com = input('Enter command:\n')
-                spectrometer.write(com)
-                response = spectrometer.read()
-                print('RES:', response)
-            except Exception as e:
-                print(e) 
-                
-
-        # 
-        # Read the response from the spectrometer
-
-        try:
-            response = spectrometer.read()
-            print("Response:", response)
-        except pyvisa .VisaIOError as e:
-            if e.error_code == pyvisa.constants.VI_ERROR_TMO:
-                print("Timeout expired. spectrometer did not respond.")
-            else:
-                print("An error occurred:", e)
-
-
-        # Close the spectrometer connection
-        spectrometer.close()
-
-
-        #k0,0,100 = move slit 100 steps relative
-        #j0,0<CR> = read slit pos
-        #i0,0,0<CR> = set slit position (0)
-
-
-# Note: pico-mk3-coms_V1.0.py is the working version. This wrapper is having issues with the com port.
-# notes re: this version: marlin controller will only work if movement commands are sent. Anything else will break the read/write loop. need to move away from this marlin controller ASAP, or write a more robust coms wrapper.
-
-
